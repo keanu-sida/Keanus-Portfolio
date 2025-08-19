0af855aa85114b5c2f7dff6485458d5083d81c6c
Keanu Rico Sida <keanu.sida@gmail.com>
2025-08-19 11:33:12 -0700

    Remove hero gradient blob from intro section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -44,7 +44,6 @@
 
 <main class="flex flex-col flex-1 p-4">
     <section id="introPage" class="relative grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14 overflow-hidden">
-        <div class="hero-blob" aria-hidden="true"></div>
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10" use:reveal>
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
                 Hi! I'm <span class="poppins text-violet-400">Keanu</span> Sida <br/>Full Stack <span class="poppins text-violet-400">Developer</span>
